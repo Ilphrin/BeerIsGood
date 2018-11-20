1ba566e320489812dc5836dc8152a7384e4c83ec
Kevin "Ilphrin" Pellet <ilphrin@autistici.org>
2018-11-20 16:38:00 +0100

    Fix incorrect lint error in sqlite.js

--- src/models/sqlite.js
+++ src/models/sqlite.js
@@ -1,8 +1,6 @@
 import { SQLite } from 'expo';
 import requests from './init/init';
 
-const db = SQLite.openDatabase('beerIsGood');
-
 function defaultErrCallback(transaction, error) {
   console.warn(`[ERR] In transaction ${transaction}: ${error}`);
 }
@@ -15,13 +13,6 @@ function request(db, callback, success = () => {}, error = () => {}) {
   db.transaction(callback, error, success);
 }
 
-function init() {
-  request(db, (tx) => {
-    for (req of requests) {
-      tx.executeSql(req, []);
-    }
-  });
-}
 
 function get_all(db, success = defaultSuccessCallback, error = defaultErrCallback) {
   request(db, tx => (
@@ -55,11 +46,21 @@ function update_beer(db, beer, success = defaultSuccessCallback, error = default
   request(db, tx => (
     tx.executeSql(
       'UPDATE beers SET name=?, type=?, brewery=?, pic=? WHERE id=?',
-      [beer.name, beer.type, beer.brewery, beer.photo, beer.id],
+      [beer.name, beer.type, beer.brewery, beer.pic, beer.id],
       success,
       error,
     )
   ));
+}
+
+const db = SQLite.openDatabase('beerIsGood');
+
+function init() {
+  request(db, (tx) => {
+    for (req of requests) {
+      tx.executeSql(req, []);
+    }
+  });
 }
 
 init();
