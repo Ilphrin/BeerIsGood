9f3061589d4808cb0db890c16c9d369864fb503d
Kevin "Ilphrin" Pellet <ilphrin@autistici.org>
2019-01-19 10:47:09 +0100

    Data version update to 1

---
+++ VERSION.md
@@ -0,0 +1,14 @@
+### BeerIsGood version
+
+
+
+### BeerIsGood data version
+
+## 0
+
+- Add a picture as text
+
+## 1
+
+- Add the SRM as integer integer
+- Add the IBU as an integer
--- src/models/init/init.js
+++ src/models/init/init.js
@@ -6,13 +6,14 @@ const requests = [
 const versionRequests = [
   [0, 'ALTER TABLE beers ADD COLUMN pic TEXT;'],
   [1, 'ALTER TABLE beers ADD COLUMN color INTEGER;'],
+  [1, 'ALTER TABLE beers ADD COLUMN ibu INTEGER;'],
 ];
 
 const commonReq = {
   getAllBeers: 'SELECT * FROM beers;',
-  newBeer: 'INSERT INTO beers (name, type, brewery, pic, color) VALUES (?, ?, ?, ?, ?);',
+  newBeer: 'INSERT INTO beers (name, type, brewery, pic, color, ibu) VALUES (?, ?, ?, ?, ?, ?);',
   rmBeer: 'DELETE FROM beers WHERE id=?',
-  updateBeer: 'UPDATE beers SET name=?, type=?, brewery=?, pic=?, color=? WHERE id=?',
+  updateBeer: 'UPDATE beers SET name=?, type=?, brewery=?, pic=?, color=?, ibu=? WHERE id=?',
   getVersion: 'SELECT * FROM version;',
   addFirstVersion: 'INSERT INTO version (version) VALUES (?);',
   updateVersion: 'UPDATE version SET version=? WHERE id=?',
